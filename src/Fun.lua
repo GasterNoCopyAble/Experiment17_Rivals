e5dd9ce8d074852c2db3b6dab7f2472ed08f5970
GasterNoCopyAble <gasternocopyabl@gmail.com>
2026-08-28 21:22:38 +0400

    Add Fun logical module

---
+++ src/Fun.lua
@@ -0,0 +1,2 @@
+-- Experiment 17 | Rivals | Fun / Notifications
+CompileLegacySection("Fun.lua", "--==================================================\n-- CUSTOM TEXTURES\n--==================================================", "--==================================================\n-- WORLD WATCHERS / BATCHED ROOM PROCESSING\n--==================================================")
