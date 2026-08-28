9e473b1da6f042504f618154bdcc829af935402b
GasterNoCopyAble <gasternocopyabl@gmail.com>
2026-08-28 21:22:30 +0400

    Add Protection logical module

---
+++ src/Protection.lua
@@ -0,0 +1,2 @@
+-- Experiment 17 | Rivals | Protection
+CompileLegacySection("Protection.lua", "--==================================================\n-- SOURCE-INSPIRED CLIENT PROTECTIONS / ROOM 50\n--==================================================", "--==================================================\n-- CUSTOM TEXTURES\n--==================================================")
