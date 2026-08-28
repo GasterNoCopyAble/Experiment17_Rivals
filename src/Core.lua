bfeeda25f052558ba539c370df2550fae26e6840
GasterNoCopyAble <gasternocopyabl@gmail.com>
2026-08-28 21:21:51 +0400

    Add Core logical module

---
+++ src/Core.lua
@@ -0,0 +1,2 @@
+-- Experiment 17 | Rivals | Core
+CompileLegacySection("Core.lua", nil, "--==================================================\n-- PROXIMITY PROMPTS\n--==================================================")
