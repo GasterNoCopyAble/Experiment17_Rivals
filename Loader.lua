23e7d25e6daf83c034d6ea6d46d01ebc5549ec1c
GasterNoCopyAble <gasternocopyabl@gmail.com>
2026-08-28 21:10:05 +0400

    Fix Rivals loader chunk list

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 -- Experiment 17 | Rivals
--- Loads the fixed Legacy v22 build from chunked sources.
+-- Legacy v22 build loader.
 
 local BASE = "https://raw.githubusercontent.com/GasterNoCopyAble/Experiment17_Rivals/main/src/chunks/"
 local PARTS = {
@@ -9,8 +9,19 @@ local PARTS = {
     "004.lua.txt",
     "005.lua.txt",
     "006.lua.txt",
-    "007.lua.txt",
-    "008.lua.txt",
+    "007-008.lua.txt",
+    "009-010.lua.txt",
+    "011-012.lua.txt",
+    "013-014.lua.txt",
+    "015-016.lua.txt",
+    "017-018.lua.txt",
+    "019-020.lua.txt",
+    "021-022.lua.txt",
+    "023.lua.txt",
+    "024.lua.txt",
+    "025.lua.txt",
+    "026.lua.txt",
+    "027.lua.txt",
 }
 
 local source = table.create(#PARTS)
